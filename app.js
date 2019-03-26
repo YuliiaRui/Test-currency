757fa3d81c7dd9aad12d475ab914333c341f2cd5
Yuliia <yuliiarui@gmail.com>
2019-03-26 22:57:22 +0200

    click js

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-let recieveBtn = document.querySelector(".recieve-button");
+
+let recieveBtn = document.querySelectorAll(".recieve-button");
 let recieve = document.querySelector(".recieve");
 let close = document.querySelector(".closeBtn");
 
-recieveBtn.addEventListener('click', show);
-function show() {
-    recieve.style.display = "block"
-}
+recieveBtn.forEach((i)=>{
+    i.addEventListener('click', show);
+    function show() {
+        recieve.style.display = "block";
+}});
+
 
 close.addEventListener('click', closeBlock);
 function closeBlock() {
     recieve.style.display = 'none'
 }
-
-
-
-
