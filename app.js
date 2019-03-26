9baa71beaff94b29c2d85ad2c5bfda27c8400965
Yuliia <yuliiarui@gmail.com>
2019-03-26 09:27:20 +0200

    close btn

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-let recieve = document.querySelector(".recieve")
+let recieveBtn = document.querySelector(".recieve-button");
+let recieve = document.querySelector(".recieve");
+let close = document.querySelector(".closeBtn");
+
+
+recieveBtn.addEventListener('click', show);
+function show() {
+    recieve.style.display = "block"
+}
+
+close.addEventListener('click', closeBlock);
+function closeBlock() {
+    recieve.style.display = 'none'
+}
